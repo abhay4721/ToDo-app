ec0a232cb4d2d531f8ed162dcba1363aba6b6e1f
abhay4721 <abhayabhi4721@gmail.com>
2025-02-20 19:40:08 +0530

    adding the router path inside the app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: TodoComponent, pathMatch: 'full' },
-  { path: 'task', component: TodoComponent }
+  { path: 'task', component: TodoComponent },
+  {
+            path:'home',
+        component: TodoComponent,
+        pathMatch:'full'
+        },
+        {
+            path:'about',
+        component: AboutComponent,
+        pathMatch:'full'
+        },
 ];
